e8f3951e9f8b39a124affe442a22420c0f1a6980
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-03-22 22:47:01 +0200

    rework button radius

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        startButton.layer.cornerRadius = 15
+        startButton.layer.cornerRadius = 10
         
         redLightView.alpha = lightIsOff
         yellowLightView.alpha = lightIsOff
